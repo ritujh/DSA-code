1fb3f2f8d0b18b6d678ff35defc4f952ae6099c9
Ritu <ritujhare@gmail.com>
2020-08-02 02:22:48 +0530

    added ques and examples

--- word-break.java
+++ word-break.java
@@ -1,3 +1,31 @@
+/**
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
+ 
+ Note:
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+ 
+Input:
+s = "catsanddog"
+wordDict = ["cat", "cats", "and", "sand", "dog"]
+Output:
+[
+  "cats and dog",
+  "cat sand dog"
+]
+
+Input:
+s = "pineapplepenapple"
+wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+Output:
+[
+  "pine apple pen apple",
+  "pineapple pen apple",
+  "pine applepen apple"
+]
+
+ **/
+
 class Solution {
    public List<String> wordBreak(String s, List<String> wordDict) {  
        HashMap<Integer, List<String>> hm = new HashMap<>(); 
