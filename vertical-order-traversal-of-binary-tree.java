805209dddd18dceb18fcbdf29e3852d7078ef339
Ritu <ritujhare@gmail.com>
2020-08-08 10:16:14 +0530

    hashtable, comparable, pq, bfs of binary tree

---
+++ vertical-order-traversal-of-binary-tree.java
@@ -0,0 +1,115 @@
+/**
+Vertical Order Traversal of a Binary Tree
+
+Given a binary tree, return the vertical order traversal of its nodes values.
+
+For each node at position (X, Y), its left and right children respectively will be at positions (X-1, Y-1) and (X+1, Y-1).
+
+Running a vertical line from X = -infinity to X = +infinity, whenever the vertical line touches some nodes, we report the values of the nodes in order from top to bottom (decreasing Y coordinates).
+
+If two nodes have the same position, then the value of the node that is reported first is the value that is smaller.
+
+Return an list of non-empty reports in order of X coordinate.  Every report will have a list of values of nodes.
+
+Input: [3,9,20,null,null,15,7]
+Output: [[9],[3,15],[20],[7]]
+Explanation: 
+Without loss of generality, we can assume the root node is at position (0, 0):
+Then, the node with value 9 occurs at position (-1, -1);
+The nodes with values 3 and 15 occur at positions (0, 0) and (0, -2);
+The node with value 20 occurs at position (1, -1);
+The node with value 7 occurs at position (2, -2).
+
+Input: [1,2,3,4,5,6,7]
+Output: [[4],[2],[1,5,6],[3],[7]]
+Explanation: 
+The node with value 5 and the node with value 6 have the same position according to the given scheme.
+However, in the report "[1,5,6]", the node value of 5 comes first since 5 is smaller than 6.
+
+leetcode link: https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3415/
+ 
+ HINT-
+ 1. consider nodes as points (node,x,y)
+ 2. Make hashtable for x->points; pq of points having same horizontal distance
+ 3. implement comparable in point, if y distance is same than compare on point value
+4. do bfs od binary tree and add points in hashtable
+
+ 
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Point implements Comparable<Point>{
+        TreeNode node;
+        int x;
+        int y;
+        public Point(TreeNode node,int x,int y){
+            this.node = node;
+            this.x = x;
+            this.y = y;
+        }
+        public int compareTo(Point o){
+            if(o.y==this.y){
+                return node.val-o.node.val;
+            }
+            return y-o.y;
+        }
+}
+class Solution {
+    
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        if(root==null){
+            return null;
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        Point p = new Point(root,0,0);
+        //bfs
+        Queue<Point> que = new LinkedList<>();
+        //HM of x and pq<point>
+        Map<Integer,PriorityQueue<Point>> hm = new TreeMap<>();
+        que.add(p);
+        while(que.isEmpty()==false){
+            Point first = que.poll();
+            TreeNode firstNode = first.node;
+            if(hm.containsKey(first.x)){
+                PriorityQueue<Point> pq = hm.get(first.x);
+                pq.add(first);
+                hm.put(first.x,pq);
+            }else{
+                PriorityQueue<Point> pq = new PriorityQueue<>();
+                pq.add(first);
+                hm.put(first.x,pq);
+            }
+            //hm.put(,hm.getOrDefault(first.x,new PriorityQueue<>()).add(first));
+            if(firstNode.left!=null){
+                que.add(new Point(firstNode.left,first.x -1,first.y+1));
+            }
+            if(firstNode.right!=null){
+                que.add(new Point(firstNode.right,first.x +1,first.y+1));
+            }
+        }
+        for(Map.Entry entry : hm.entrySet()){
+            List<Integer> list = new ArrayList<>();
+            PriorityQueue<Point> pq = (PriorityQueue) entry.getValue();
+            while(pq.isEmpty()==false){
+                Point p1 = pq.poll();
+                list.add(p1.node.val);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
